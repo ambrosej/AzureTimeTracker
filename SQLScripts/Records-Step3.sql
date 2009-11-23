96428eb94346281f969f8ed36a415fa0f6236e1e
~jeyaprabhujeyaprakash <jeyaprabhu.j@proteans.com>
2009-11-23 11:24:34 +0530

    SQLScripts First Version from Mahesh

---
+++ SQLScripts/Records-Step3.sql
@@ -0,0 +1,9 @@
+
+
+
+insert into aspnet_schemaversions values ('common' ,1, 1)
+insert into aspnet_schemaversions values ('role manager' ,1, 1)
+insert into aspnet_schemaversions values ('health monitoring' ,1, 1)
+insert into aspnet_schemaversions values ('membership' ,1, 1)
+insert into aspnet_schemaversions values ('personalization' ,1, 1)
+insert into aspnet_schemaversions values ('profile' ,1, 1)
